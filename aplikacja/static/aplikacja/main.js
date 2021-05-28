6456144a54c0d32ee3a4adb59131f3a0b8299dc4
Filip <filip.korput@gmail.com>
2021-05-28 11:13:18 +0200

    s

--- aplikacja/static/aplikacja/main.js
+++ aplikacja/static/aplikacja/main.js
@@ -171,7 +171,7 @@ window.onload = () => {
                  $('.focus').empty()
 
                  for (const section of response.sectionList) {
-                     $('<div>').addClass('section').appendTo($('.focus')).append($('<button>').addClass('collapsible').html(section[2] + " - " + section[1])).append($('<div>').addClass('section_content').html(section[0])).append($('<p>').text("------------------------------------------------------------"));
+                     $('<div/>', { 'class': "section" }).appendTo($('.focus')).append($('<button>').addClass('collapsible').html(section[2] + " - " + section[1])).append($('<div>').addClass('section_content').html(section[0])).append($('<p>').text("------------------------------------------------------------"));
                  }
              }
          });
